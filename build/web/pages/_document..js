f92fbe6de96b2272a53dffdb906d5caadfe17a51
Federico Corradi <corradi.federico.91@gmail.com>
2020-08-25 14:04:35 +0100

    Update _document..js
    Removed google analytics

--- build/web/pages/_document..js
+++ build/web/pages/_document..js
@@ -6,20 +6,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172182289-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'UA-172182289-1');
-            </script>
-            `,
-          }}
-        />
           <link rel="preload" href="/fonts/Altero-Regular.otf" as="font" crossOrigin=""/>
         </Head>
         <body>
