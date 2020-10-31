330b1e508f069582c16a0c6eafbbc27353860881
Federico Corradi <corradi.federico.91@gmail.com>
2020-10-31 11:52:46 +0000

    Added fix for page transition styling

---
+++ build/web/pages/404.js
@@ -0,0 +1,10 @@
+
+const Custom404 = () => {
+    return (
+        <div className="error">
+            <h1>404 - Page Not Found</h1>
+        </div>
+    )
+}
+
+export default Custom404;
--- build/web/pages/_app.js
+++ build/web/pages/_app.js
@@ -1,7 +1,8 @@
 import Cursor from '../components/UI/Cursor/Cursor';
 import hoverElements from '../util/cursor-context';
 import Layout from '../components/Layout/Layout';
-import { PageTransition } from 'next-page-transitions'
+import { AnimatePresence } from 'framer-motion';
+import Router from "next/router";
 import App from 'next/app';
 import React from 'react';
 import './app.css';
@@ -13,12 +14,31 @@ class MyApp extends App {
     return (
       <Layout>
         <Cursor />
-          <PageTransition timeout={ 1000 } classNames={"page-transition"}>
-            <Component { ...pageProps } key={ router.asPath }></Component>
-          </PageTransition>
+        <AnimatePresence exitBeforeEnter onExitComplete={ () => window.scrollTo( 0, 0) }>
+          <Component { ...pageProps } key={ router.asPath }></Component>
+        </AnimatePresence>
       </Layout>
     )
   }
+
 }
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange );
+Router.events.on("routeChangeStart", routeChange );
 
 export default MyApp
