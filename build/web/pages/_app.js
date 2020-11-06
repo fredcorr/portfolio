c178a003636be3486a928356b255dc8d84cfb22d
Federico Corradi <corradi.federico.91@gmail.com>
2020-11-06 19:15:31 +0000

    Removed Page Transition / Reduce int trigger

--- build/web/pages/_app.js
+++ build/web/pages/_app.js
@@ -2,7 +2,6 @@ import Cursor from '../components/UI/Cursor/Cursor';
 import hoverElements from '../util/cursor-context';
 import Layout from '../components/Layout/Layout';
 import { AnimatePresence } from 'framer-motion';
-import Router from "next/router";
 import App from 'next/app';
 import React from 'react';
 import './app.css';
@@ -14,31 +13,11 @@ class MyApp extends App {
     return (
       <Layout>
         <Cursor />
-        <AnimatePresence exitBeforeEnter onExitComplete={ () => window.scrollTo( 0, 0) }>
-          <Component { ...pageProps } key={ router.asPath }></Component>
-        </AnimatePresence>
+        <Component { ...pageProps } key={ router.asPath }></Component>
       </Layout>
     )
   }
 
 }
-
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
-
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
-};
-
-Router.events.on("routeChangeComplete", routeChange );
-Router.events.on("routeChangeStart", routeChange );
 
 export default MyApp
